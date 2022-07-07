5d488c1a4216872bbc8ecd6feb7241da0d540e09
DengZhuo <63216977+dengzhuo-AI@users.noreply.github.com>
2022-07-07 15:00:11 +0800

    Update RF_dataset.py

--- train_code/architecture/RF_dataset.py
+++ train_code/architecture/RF_dataset.py
@@ -1,1 +1,125 @@
+import numpy as np
+import os
+from torch.utils.data import Dataset
+import torch
+from utils import load_img, Augment_RGB_torch
+import torch.nn.functional as F
+import random
+import pdb
 
+augment   = Augment_RGB_torch()
+transforms_aug = [method for method in dir(augment) if callable(getattr(augment, method)) if not method.startswith('_')] 
+
+class DataLoaderTrain_RF(Dataset):
+    def __init__(self, rgb_dir, img_options=None, target_transform=None):
+        super(DataLoaderTrain_RF, self).__init__()
+
+        self.target_transform = target_transform
+
+        rgb_dir = '/data/dengz/trian_dataset/trian/'
+
+        gt_files = sorted(os.listdir(os.path.join(rgb_dir, 'gt')))
+        input_files = sorted(os.listdir(os.path.join(rgb_dir, 'input')))
+        
+        self.gt_filenames = [os.path.join(rgb_dir, 'gt', x) for x in gt_files]
+        self.input_filenames = [os.path.join(rgb_dir, 'input', x) for x in input_files]
+        
+        self.img_options=img_options
+
+        self.tar_size = len(self.gt_filenames)  # get the size of target
+
+    def __len__(self):
+        return self.tar_size
+
+    def __getitem__(self, index):
+        tar_index   = index % self.tar_size
+        gt = torch.from_numpy(np.float32(load_img(self.gt_filenames[tar_index])))
+        input = torch.from_numpy(np.float32(load_img(self.input_filenames[tar_index])))
+        
+        gt = gt.permute(2,0,1)
+        input = input.permute(2,0,1)
+
+        gt_filename = os.path.split(self.gt_filenames[tar_index])[-1]
+        input_filename = os.path.split(self.input_filenames[tar_index])[-1]
+
+
+        apply_trans = transforms_aug[random.getrandbits(3)]
+
+        gt = getattr(augment, apply_trans)(gt)
+        input = getattr(augment, apply_trans)(input)        
+
+        return gt, input, gt_filename, input_filename
+  
+class DataLoaderVal_RF(Dataset):
+    def __init__(self, rgb_dir, target_transform=None):
+        super(DataLoaderVal_RF, self).__init__()
+
+        self.target_transform = target_transform
+
+        rgb_dir = '/data/dengz/trian_dataset/val'
+
+
+        gt_files = sorted(os.listdir(os.path.join(rgb_dir, 'gt')))
+        input_files = sorted(os.listdir(os.path.join(rgb_dir, 'input')))
+
+
+        self.gt_filenames = [os.path.join(rgb_dir, 'gt', x) for x in gt_files]
+        self.input_filenames = [os.path.join(rgb_dir, 'input', x) for x in input_files]
+        
+
+        self.tar_size = len(self.gt_filenames)  
+
+    def __len__(self):
+        return self.tar_size
+
+    def __getitem__(self, index):
+        tar_index   = index % self.tar_size
+        
+        gt = torch.from_numpy(np.float32(load_img(self.gt_filenames[tar_index])))
+        input = torch.from_numpy(np.float32(load_img(self.input_filenames[tar_index])))
+                
+        gt_filename = os.path.split(self.gt_filenames[tar_index])[-1]
+        input_filename = os.path.split(self.input_filenames[tar_index])[-1]
+
+        gt = gt.permute(2,0,1)
+        input = input.permute(2,0,1)
+
+        return gt, input, gt_filename, input_filename
+####################################################################################################
+
+class  DataLoaderTes_RF(Dataset):
+    def __init__(self, rgb_dir, target_transform=None):
+        super(DataLoaderTes_RF, self).__init__()
+
+        self.target_transform = target_transform
+
+#        rgb_dir = '/data/dengz/xujiao_dataset/val'
+        rgb_dir = '/data/dengz/test30_1280'
+#        rgb_dir = '/data/dengz/ri/'
+        gt_files = sorted(os.listdir(os.path.join(rgb_dir, 'gt')))
+        input_files = sorted(os.listdir(os.path.join(rgb_dir, 'input')))
+
+
+        self.gt_filenames = [os.path.join(rgb_dir, 'gt', x) for x in gt_files]
+        self.input_filenames = [os.path.join(rgb_dir, 'input', x) for x in input_files]
+        
+
+        self.tar_size = len(self.gt_filenames)  
+
+    def __len__(self):
+        return self.tar_size
+
+    def __getitem__(self, index):
+        tar_index   = index % self.tar_size
+        
+
+        gt = torch.from_numpy(np.float32(load_img(self.gt_filenames[tar_index])))
+        input = torch.from_numpy(np.float32(load_img(self.input_filenames[tar_index])))
+                
+        gt_filename = os.path.split(self.gt_filenames[tar_index])[-1]
+        input_filename = os.path.split(self.input_filenames[tar_index])[-1]
+
+        gt = gt.permute(2,0,1)
+        input = input.permute(2,0,1)
+
+        return gt, input, gt_filename, input_filename
