4467cc6a51e12b8174ba9eb2e2221678545a6fb6
DengZhuo <63216977+dengzhuo-AI@users.noreply.github.com>
2022-07-07 15:21:40 +0800

    Update utils.py

--- train_code/utils.py
+++ train_code/utils.py
@@ -1,1 +1,37 @@
+import torch
+import numpy as np
+import pickle
+import cv2
 
+def load_img(filepath):
+    img = cv2.cvtColor(cv2.imread(filepath), cv2.COLOR_BGR2RGB)
+    img = img.astype(np.float32)
+    img = img/255.
+    return img
+
+class Augment_RGB_torch:
+    def __init__(self):
+        pass
+    def transform0(self, torch_tensor):
+        return torch_tensor   
+    def transform1(self, torch_tensor):
+        torch_tensor = torch.rot90(torch_tensor, k=1, dims=[-1,-2])
+        return torch_tensor
+    def transform2(self, torch_tensor):
+        torch_tensor = torch.rot90(torch_tensor, k=2, dims=[-1,-2])
+        return torch_tensor
+    def transform3(self, torch_tensor):
+        torch_tensor = torch.rot90(torch_tensor, k=3, dims=[-1,-2])
+        return torch_tensor
+    def transform4(self, torch_tensor):
+        torch_tensor = torch_tensor.flip(-2)
+        return torch_tensor
+    def transform5(self, torch_tensor):
+        torch_tensor = (torch.rot90(torch_tensor, k=1, dims=[-1,-2])).flip(-2)
+        return torch_tensor
+    def transform6(self, torch_tensor):
+        torch_tensor = (torch.rot90(torch_tensor, k=2, dims=[-1,-2])).flip(-2)
+        return torch_tensor
+    def transform7(self, torch_tensor):
+        torch_tensor = (torch.rot90(torch_tensor, k=3, dims=[-1,-2])).flip(-2)
+        return torch_tensor  
