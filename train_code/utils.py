8f450fb4ccd82d02cc0725bf0242e6290ce24d5d
DengZhuo <63216977+dengzhuo-AI@users.noreply.github.com>
2022-07-07 16:29:31 +0800

    Update utils.py

--- train_code/utils.py
+++ train_code/utils.py
@@ -2,6 +2,8 @@ import torch
 import numpy as np
 import pickle
 import cv2
+import logging
+import os
 
 def load_img(filepath):
     img = cv2.cvtColor(cv2.imread(filepath), cv2.COLOR_BGR2RGB)
@@ -35,3 +37,59 @@ class Augment_RGB_torch:
     def transform7(self, torch_tensor):
         torch_tensor = (torch.rot90(torch_tensor, k=3, dims=[-1,-2])).flip(-2)
         return torch_tensor  
+
+
+def get_logger(name='', save_dir=None,filename="log.txt",verbosity=1):
+    level_dict = {0: logging.DEBUG, 1: logging.INFO, 2: logging.WARNING}
+    formatter = logging.Formatter(
+        "[%(asctime)s][%(filename)s][line:%(lineno)d][%(levelname)s] %(message)s"
+    )
+    logger = logging.getLogger(name)
+    logger.setLevel(level_dict[verbosity])
+
+    fh = logging.FileHandler(os.path.join(save_dir, filename), "w")
+    fh.setFormatter(formatter)
+    logger.addHandler(fh)
+
+    sh = logging.StreamHandler()
+    sh.setFormatter(formatter)
+    logger.addHandler(sh)
+
+    return logger
+
+def setup_log(name='train', log_dir=None, file_name=None):
+    logger = get_logger(name=name, save_dir=log_dir, filename=file_name)
+    return logger
+
+class MixUp_AUG:
+    def __init__(self):
+        self.dist = torch.distributions.beta.Beta(torch.tensor([1.2]), torch.tensor([1.2]))
+
+    def aug(self, rgb_gt, rgb_noisy):
+        bs = rgb_gt.size(0)
+        indices = torch.randperm(bs)
+        rgb_gt2 = rgb_gt[indices]
+        rgb_noisy2 = rgb_noisy[indices]
+
+        lam = self.dist.rsample((bs,1)).view(-1,1,1,1).cuda()
+
+        rgb_gt    = lam * rgb_gt + (1-lam) * rgb_gt2
+        rgb_noisy = lam * rgb_noisy + (1-lam) * rgb_noisy2
+
+        return rgb_gt, rgb_noisy
+    
+def save_img(filepath, img):
+    cv2.imwrite(filepath,cv2.cvtColor(img, cv2.COLOR_RGB2BGR))
+
+def myPSNR(tar_img, prd_img):
+    imdff = torch.clamp(prd_img,0,1) - torch.clamp(tar_img,0,1)
+    rmse = (imdff**2).mean().sqrt()
+    ps = 20*torch.log10(1/rmse)
+    return ps
+
+def batch_PSNR(img1, img2, data_range=None):
+    PSNR = []
+    for im1, im2 in zip(img1, img2):
+        psnr = myPSNR(im1, im2)
+        PSNR.append(psnr)
+    return sum(PSNR)/len(PSNR)
