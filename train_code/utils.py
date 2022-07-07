3bf47569e9b2d722bf8f5a28073c07d365938bea
DengZhuo <63216977+dengzhuo-AI@users.noreply.github.com>
2022-07-07 16:32:31 +0800

    Update utils.py

--- train_code/utils.py
+++ train_code/utils.py
@@ -4,6 +4,23 @@ import pickle
 import cv2
 import logging
 import os
+from natsort import natsorted
+from glob import glob
+
+def mkdirs(paths):
+    if isinstance(paths, list) and not isinstance(paths, str):
+        for path in paths:
+            mkdir(path)
+    else:
+        mkdir(paths)
+
+def mkdir(path):
+    if not os.path.exists(path):
+        os.makedirs(path)
+
+def get_last_path(path, session):
+	x = natsorted(glob(os.path.join(path,'*%s'%session)))[-1]
+	return x
 
 def load_img(filepath):
     img = cv2.cvtColor(cv2.imread(filepath), cv2.COLOR_BGR2RGB)
